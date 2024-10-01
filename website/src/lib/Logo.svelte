c2e3b182694fb8c6c53ed634f09d62e26c4a384b
cielaton <vuwin24680@gmail.com>
2024-10-01 16:27:23 +0700

    Added the Sponsor and "Doan" logo.

--- website/src/lib/Logo.svelte
+++ website/src/lib/Logo.svelte
@@ -3,15 +3,18 @@
 
 <div style:height="100%" style:display="flex" style:aspect-ratio="11/4">
 	<div id="image_wrapper">
+    <!-- List all of the logos -->
 		<img style:height="80px" src="$lib/assets/BKLogo.jpg" />
+		<img class="separate_image" src="$lib/assets/DoanLogo.png" />
 		<img class="separate_image" src="$lib/assets/FASTLogo.png" />
+		<img id="sponsor_logo" src="$lib/assets/UniquifyLogo.png" />
 	</div>
 </div>
 
 <style>
 	.separate_image {
 		padding-left: 20px;
-    height: 100px;
+		height: 100px;
 	}
 
 	#image_wrapper {
@@ -20,6 +23,11 @@
 		width: auto;
 		height: 100%;
 		justify-content: center;
-    align-items: center;
+		align-items: center;
 	}
+
+  #sponsor_logo {
+    height: 60px;
+    padding-left: 50px;
+  }
 </style>
