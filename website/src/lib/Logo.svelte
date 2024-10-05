222d3a30cfb389e8e6d88c709fb9da43253f8167
cielaton <vuwin24680@gmail.com>
2024-10-05 21:52:30 +0700

    Fixed background display issue on mobile view.

--- website/src/lib/Logo.svelte
+++ website/src/lib/Logo.svelte
@@ -1,33 +1,66 @@
 <script lang="ts">
 </script>
 
-<div style:height="100%" style:display="flex" style:aspect-ratio="11/4">
-	<div id="image_wrapper">
-    <!-- List all of the logos -->
-		<img style:height="80px" src="$lib/assets/BKLogo.jpg" />
-		<img class="separate_image" src="$lib/assets/DoanLogo.png" />
-		<img class="separate_image" src="$lib/assets/FASTLogo.png" />
-		<img id="sponsor_logo" src="$lib/assets/UniquifyLogo.png" />
-	</div>
+<div style:height="100%" style:width="100%" style:display="flex">
+    <div id="image_wrapper">
+        <!-- List all of the logos -->
+        <img id="BKLogo" src="$lib/assets/BKLogo.jpg"/>
+        <img id="DoanLogo" src="$lib/assets/DoanLogo.png"/>
+        <img id="FASTLogo" src="$lib/assets/FASTLogo.png"/>
+        <img id="UniquifyLogo" src="$lib/assets/UniquifyLogo.png"/>
+    </div>
 </div>
 
 <style>
-	.separate_image {
-		padding-left: 20px;
-		height: 100px;
-	}
+    #image_wrapper {
+        padding: 0 20px 0 20px;
+        margin-right: auto;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        justify-content: space-between;
+        align-items: center;
+    }
 
-	#image_wrapper {
-		margin-right: auto;
-		display: flex;
-		width: auto;
-		height: 100%;
-		justify-content: center;
-		align-items: center;
-	}
+    #BKLogo {
+        width: 15%;
+    }
 
-  #sponsor_logo {
-    height: 60px;
-    padding-left: 30px;
-  }
+    #DoanLogo {
+        width: 15%;
+    }
+
+    #FASTLogo {
+        width: 30%;
+    }
+
+    #UniquifyLogo {
+        width: 30%;
+    }
+
+    @media only screen and (min-width: 525px) {
+        #BKLogo {
+            height: 60px;
+            width: auto;
+            padding-left: 20px;
+        }
+
+        #DoanLogo {
+            height: 60px;
+            width: auto;
+            padding-left: 20px;
+        }
+
+        #FASTLogo {
+            height: 100px;
+            width: auto;
+            padding-left: 20px;
+        }
+
+        #UniquifyLogo {
+            height: 50px;
+            width: auto;
+            padding-left: 20px;
+        }
+    }
 </style>
